d1a635e05a666421b826ff0f67321ff4dd7c7928
dx-mon <dx-mon@users.sourceforge.net>
2021-04-15 03:36:03 +0100

    Begun implementing the layout engine for standardTextLayout_t

--- include/contort/textLayout.hxx
+++ include/contort/textLayout.hxx
@@ -3,16 +3,29 @@
 #define CONTORT_TEXT_LAYOUT__HXX
 
 #include <string_view>
+#include <stdexcept>
+#include <vector>
 #include <contort/widget.hxx>
 
 namespace contort
 {
+	struct cantDisplayText_t final : std::exception
+	{
+	public:
+		const char *what() const noexcept final { return ""; }
+	};
+
+	struct layout_t { };
+
+	using segment_t = std::tuple<size_t, size_t>;
+	using segments_t = std::vector<std::vector<segment_t>>;
+
 	struct textLayout_t
 	{
 		virtual ~textLayout_t() noexcept = default;
 		virtual bool supportsAlignMode(horizontalAlignment_t) const noexcept { return true; }
 		virtual bool supportsWrapMode(wrapping_t) const noexcept { return true; }
-		virtual bool layout(std::string_view text, uint32_t width,
+		virtual std::vector<layout_t> layout(std::string_view text, uint32_t width,
 			horizontalAlignment_t align, wrapping_t wrap) const noexcept = 0;
 	};
 
@@ -21,8 +34,10 @@ namespace contort
 		~standardTextLayout_t() noexcept final = default;
 		bool supportsAlignMode(horizontalAlignment_t align) const noexcept final;
 		bool supportsWrapMode(wrapping_t wrap) const noexcept final;
-		bool layout(std::string_view text, uint32_t width,
+		std::vector<layout_t> layout(std::string_view text, uint32_t width,
 			horizontalAlignment_t align, wrapping_t wrap) const noexcept final;
+
+		segments_t calculateTextSegments(std::string_view text, uint32_t width, wrapping_t wrap) const;
 	};
 
 	using defaultLayout_t = standardTextLayout_t;
--- src/textLayout.cxx
+++ src/textLayout.cxx
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: BSD-3-Clause
 #include <contort/textLayout.hxx>
+#include <contort/utils.hxx>
 
 namespace contort
 {
@@ -27,4 +28,49 @@ namespace contort
 
 	bool standardTextLayout_t::supportsWrapMode(const wrapping_t wrap) const noexcept
 		{ return wrapValueIs_t<wrapping_t::any, wrapping_t::space, wrapping_t::clip, wrapping_t::ellipsis>{}(wrap); }
+
+	std::vector<layout_t> standardTextLayout_t::layout(const std::string_view text, const uint32_t width,
+		const horizontalAlignment_t align, const wrapping_t wrap) const noexcept try
+	{
+		const auto segments{calculateTextSegments(text, width, wrap)};
+
+		return {};
+	}
+	catch (const cantDisplayText_t &)
+		{ return {}; }
+
+	segments_t standardTextLayout_t::calculateTextSegments(const std::string_view text,
+		const uint32_t width, const wrapping_t wrap) const
+	{
+		if (wrapValueIs_t<wrapping_t::clip, wrapping_t::ellipsis>{}(wrap))
+		{
+			segments_t segments{};
+			for (size_t i = 0; i < text.length(); )
+			{
+				std::vector<segment_t> segment{};
+				auto lineEnd{[=]()
+				{
+					const auto end{text.find('\n', i)};
+					if (end == std::string_view::npos)
+						return text.length() - 1;
+					return end;
+				}()};
+				const auto screenCols{utils::calcWidth(text, i, lineEnd)};
+
+				const auto trimmed{wrap == wrapping_t::ellipsis && screenCols > width};
+				if (trimmed)
+				{
+				}
+				else
+				{
+				}
+
+				segments.emplace_back(std::move(segment));
+				i = lineEnd + 1;
+			}
+			return segments;
+		}
+
+		return {};
+	}
 } // namespace contort

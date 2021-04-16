e4a2a115913bbac8c79149fa306f1e1711ff4809
dx-mon <dx-mon@users.sourceforge.net>
2021-04-16 03:32:40 +0100

    Plumped the event loop into mainLoop_t

--- include/contort/mainLoop.hxx
+++ include/contort/mainLoop.hxx
@@ -3,9 +3,11 @@
 #define CONTORT_MAIN_LOOP__HXX
 
 #include <optional>
+#include <memory>
 #include <contort/defs.hxx>
 #include <contort/widget.hxx>
 #include <contort/screen.hxx>
+#include <contort/eventLoop.hxx>
 
 namespace contort
 {
@@ -14,9 +16,11 @@ namespace contort
 	private:
 		widget_t *widget_;
 		screen_t screen_;
+		std::unique_ptr<eventLoop_t> eventLoop_;
 
 	public:
-		mainLoop_t(widget_t *widget, std::optional<screen_t> screen = std::nullopt) noexcept;
+		mainLoop_t(widget_t *widget, std::optional<screen_t> screen = std::nullopt,
+			std::unique_ptr<eventLoop_t> eventLoop = {}) noexcept;
 	};
 } // namespace contort
 
--- src/mainLoop.cxx
+++ src/mainLoop.cxx
@@ -3,10 +3,14 @@
 
 namespace contort
 {
-	mainLoop_t::mainLoop_t(widget_t *const widget, std::optional<screen_t> screen) noexcept :
-		widget_{widget}, screen_{screen ? std::move(*screen) : screen_t{}}
+	mainLoop_t::mainLoop_t(widget_t *const widget, std::optional<screen_t> screen,
+		std::unique_ptr<eventLoop_t> eventLoop) noexcept : widget_{widget},
+		screen_{screen ? std::move(*screen) : screen_t{}}, eventLoop_{std::move(eventLoop)}
 	{
 		/*if (palette)
 			screen.registerPalette(palette);*/
+
+		if (!eventLoop_)
+			eventLoop_ = std::make_unique<selectEventLoop_t>();
 	}
 } // namespace contort
